da6a0220fa4b0959e67484233c4108e8092f82e7
Ayato-kosaka <kosaka.ayato@gmail.com>
2025-03-27 22:21:23 +0000

    WebSocket受信時に通知のバリデーションを入れる

--- app/alertbox.tsx
+++ app/alertbox.tsx
@@ -52,6 +52,7 @@ export default function AlertBox() {
 
       socket.onmessage = (event) => {
         const data: NotificationData = JSON.parse(event.data);
+        if (typeof data.type !== 'string') sendLog("websocketInvalidNotificationReceived", data);
         sendLog("websocketMessageReceived", data); // 👈 受信ログ
         setNotificationQueue((prevQueue) => [...prevQueue, data]);
       };
