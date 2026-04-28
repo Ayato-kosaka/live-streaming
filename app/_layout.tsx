b5396cb7d6b375998aafbd3e44702ecc81b9afce
Ayato-kosaka <kosaka.ayato@gmail.com>
2026-04-28 11:58:21 +0000

    fix #62 ThemeProvider があると、Androi 版 PRISMで黒背景が表示される。原因不明。とりあえず ThemeProvider を外す。

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,13 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     "Kosugi Maru": require("../assets/fonts/KosugiMaru-Regular.ttf"),
@@ -33,16 +24,13 @@ export default function RootLayout() {
     return null;
   }
 
-  let defaultTheme = DefaultTheme;
-  defaultTheme.colors.background = "#fffffff";
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : defaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    // <ThemeProvider value={colorScheme === "dark" ? DarkTheme : defaultTheme}>
+    // bug #62 ThemeProvider があると、Androi 版 PRISMで黒背景が表示される。原因不明。とりあえず ThemeProvider を外す。
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+    // </ThemeProvider>
   );
 }
