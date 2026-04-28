5900d945f47bf9f796eeba91e5b0a16d83edb0a8
Ayato-kosaka <kosaka.ayato@gmail.com>
2026-04-28 12:37:22 +0000

    #62 DarkThemeでもDefaultThemeでもbackgroundがtransparentになるようにする。

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,22 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     "Kosugi Maru": require("../assets/fonts/KosugiMaru-Regular.ttf"),
@@ -17,6 +26,19 @@ export default function RootLayout() {
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
+
+      // #62 bug fix: Androi 版 PRISMで黒背景が表示される。原因不明。
+      // とりあえず、全ての div の背景色を透明にすることで対処。
+      // setTimeout(() => {
+      //   if (typeof document !== "undefined") {
+      //     document.querySelectorAll("div").forEach((div) => {
+      //       const bg = getComputedStyle(div).backgroundColor;
+      //       if (bg !== "rgba(0, 0, 0, 0)") {
+      //         div.style.backgroundColor = "transparent";
+      //       }
+      //     });
+      //   }
+      // }, 100);
     }
   }, [loaded]);
 
@@ -24,13 +46,26 @@ export default function RootLayout() {
     return null;
   }
 
+  let defaultTheme = DefaultTheme;
+  defaultTheme.colors.background = "#fffffff";
+
   return (
-    // <ThemeProvider value={colorScheme === "dark" ? DarkTheme : defaultTheme}>
-    // bug #62 ThemeProvider があると、Androi 版 PRISMで黒背景が表示される。原因不明。とりあえず ThemeProvider を外す。
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-    // </ThemeProvider>
+    // bug #62 fix: Androi 版 PRISMで黒背景が表示される。原因不明。
+    // DarkThemeでもDefaultThemeでもbackgroundがtransparentになるようにする。
+    <ThemeProvider
+      value={{
+        ...(colorScheme === "dark" ? DarkTheme : DefaultTheme),
+        colors: {
+          ...(colorScheme === "dark" ? DarkTheme : DefaultTheme).colors,
+          background: "transparent",
+        },
+      }}
+    >
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
